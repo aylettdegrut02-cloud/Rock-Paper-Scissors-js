1bd1f55785979595dac36b3797523db09fa3427d
Aylett <236073384+aylettdegrut02-cloud@users.noreply.github.com>
2025-11-29 16:48:58 +1100

    Created function using Math.random to pick a number within range 0,3 exclusive and Math.floor to round, then using this value if, else if and else statements inside the getComputerChoice function define the computers choice and output it to console.log.

--- projectone.js
+++ projectone.js
@@ -1,1 +1,14 @@
-function getComputerChoice(){}
+const choice = Math.floor(Math.random() * 3);
+function getComputerChoice(choice) {
+    if (choice === 2) {
+       return "Scissors"
+    } else if (choice === 1) {
+        return "Paper"
+    } else {
+        return "Rock"
+    }
+}
+
+let compChoice = getComputerChoice(choice)
+
+console.log(compChoice)
